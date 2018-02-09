ec7a2322465356c14fbddfa1b22471846ea9b19b
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-02-08 22:18:46 -0800

    basic functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,21 @@
 import types from './types';
+
+export function getRanNum(){
+    return{
+        type: types.GET_RAN_NUMBER,
+        payload: Math.floor(Math.random() * 10) + 1
+    }
+}
+
+export function userInput(e){
+    return {
+        type: types.USER_INPUT,
+        payload: e.target.value
+    }
+}
+
+export function makeGuess(){
+    return {
+        type: types.MAKE_GUESS
+    }
+}
